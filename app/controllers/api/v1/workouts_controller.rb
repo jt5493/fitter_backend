6e798d3904fefb60bbab8195b2bcc7927de395d8
jt5493 <jt5493@hotmail.com>
2020-09-02 09:17:09 -0700

    added serializer to create route

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::WorkoutsController < ApplicationController
     def create
         workout = Workout.new(workout_params)
         if workout.save
-            render json: workout, status: :accepted
+            render json: WorkoutSerializer.new(workout), status: :accepted
         else
             render json: {errors: workout.errors.full_messages}, status: :unprocessible_entity
         end
